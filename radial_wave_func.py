e996a18566650c3ddffc868efd5931c1551878e7
cyruskyra <cyrus.wang.nian.yu@gmail.com>
2017-02-26 03:06:06 +0800

    radial_wave_func
    doesn't output correctly, please debug

---
+++ radial_wave_func.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 26 00:03:15 2017
+
+@author: Kyra
+"""
+
+def l00(x):
+    return 1
+def l10(x):
+    return 1
+def l20(x):
+    return 2
+def l30(x):
+    return 6
+def l01(x):
+    return 1 - x
+def l11(x):
+    return 4 - 2*x
+def l21(x):
+    return -6*x + 18
+def l31(x):
+    return 96 - 24*x
+def l02(x):
+    return x**2 - 4*x + 2
+def l12(x):
+    return 3*x**2 - 18*x + 18
+def l22(x):
+    return 12*x**2 - 96*x + 144
+def l32(x):
+    return 60*x**2 - 600*x + 1200
+def l03(x):
+    return -1*x**3 + 9*x**2 - 18*x + 6
+def l13(x):
+    return -4*x**3 + 48*x**2 - 144*x + 96
+def l23(x):
+    return -20*x**3 + 300*x**2 - 1200*x + 1200
+def l33(x):
+    return -120*x**3 + 2160*x**2 - 10800*x + 14400
+
+
+
+def assoc_laguerre(p,qmp):
+    if p==0:
+        if qmp==0:
+            return l00
+        elif qmp==1:
+            return l01
+        elif qmp==2:
+            return l02
+        elif qmp==3:
+            return l03
+    elif p==1:
+        if qmp==0:
+            return l10
+        if qmp==1:
+            return l11
+        if qmp==2:
+            return l12
+        if qmp==3:
+            return l13
+    elif p==2:
+        if qmp==0:
+            return l20
+        if qmp==1:
+            return l21
+        if qmp==2:
+            return l22
+        if qmp==3:
+            return l23
+    elif p==3:
+        if qmp==0:
+            return l30
+        if qmp==1:
+            return l31
+        if qmp==2:
+            return l32
+        if qmp==3:
+            return l33
+
+import scipy.constants as c
+import numpy as np 
+a=c.physical_constants['Bohr radius'][0]
+
+def radial_wave_func(n,l,r):
+    print a
+    p = 2*l + 1
+    qmp = n - l - 1
+    L = assoc_laguerre(p,qmp)
+    Lx = np.float64(L((2*r)/(n*a)))
+    fac1 = np.float64(np.math.factorial(n - l - 1))
+    print fac1
+    fac2 = np.float64(np.math.factorial(n + l))
+    print fac2
+    exp = np.float64(np.exp(r/(n*a)))
+    print exp
+    sqrt = np.sqrt(((2 / (n * a))**3) * (fac1/((2*n)*((fac2)**3))))
+    print sqrt
+    radialSolution = (sqrt * (exp) * ((2 * r) / (n * a))**l * (Lx)) / a**(- 3/2)
+    print type(radialSolution)
+    return np.round(radialSolution,5)
+    
+print radial_wave_func(1,0,a)
